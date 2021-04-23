5707c7b09f9b77116e0d6f7d3f06d8c1f5d3d575
Lailson Bento <lailsonalves95@gmail.com>
2021-04-23 00:21:23 -0300

    fixing level up modal

--- src/app/components/levelup-modal/levelup-modal.component.ts
+++ src/app/components/levelup-modal/levelup-modal.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-levelup-modal',
@@ -6,6 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./levelup-modal.component.scss'],
 })
 export class LevelupModalComponent implements OnInit {
+
+  @Input() level: number;
+
   constructor() {}
 
   ngOnInit(): void {}
